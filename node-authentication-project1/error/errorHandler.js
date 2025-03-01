98c4a2d30b304a2452ad70818c4708ed2581c743
zeeshan7417 <zeeshan7417@gmail.com>
2025-03-01 15:23:21 +0530

    Removed node-folder from the repository

--- node-authentication-project1/error/errorHandler.js
+++
@@ -1,9 +0,0 @@
-class CustomError extends Error {
-
-   constructor(message, statusCode){
-            this.message = message;
-            this.name = this.constructor.name;
-            this.statusCode = statusCode;
-      Error.captureStackTrace(this, this.constructor)
-   }
-}
